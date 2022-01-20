cb8f7b43db0f0e42010d7bfa3fdb5b7636e509ec
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 11:58:22 +0900

    Person.java

--- Person.java
+++ Person.java
@@ -1,18 +1,23 @@
 class Person {
     public String name;
     public int age;
-    public final Health health;
+    //課題3
+    //Healthを宣言
+    //public final Health health;
 
-    //課題2
+    //課題2(Bot)
     //firstName,lastName,ageを引数としたコンストラクタ
     Person(String firstName, String lastName, int age) {
-
+        this.name = firstName + " " + lastName;
+        this.age = age;
+        //課題3
+        //healthを初期化
+        //this.health =
     }
 
     //課題3
     //healthを使って身長・体重のデータを更新する
-    void updateHealth(double height, double weight) {
-
-    }
-
+    //void updateHealth(double height, double weight) {
+    //
+    //}
 }
